14485414f85d94b1a06729d09a995df645901dd7
Glitch (hello-express) <none>
2024-12-18 09:16:38 +0000

    🍀🚁 Checkpoint ./menu.html:30726734/3764 ./home.html:30726734/27 ./app.js:30726734/372 ./main.js:30726734/188 ./header.html:30726734/144

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+fetch('menu.html')
+  .then(response => response.text())
+  .then(data => {
+    const menuContainer = document.getElementById('menu-container'); 
+    menuContainer.innerHTML = data; 
+  });
+
 $(document).ready(function() {
   $('.menu-button').click(function() {
   $(this).toggleClass('open');
