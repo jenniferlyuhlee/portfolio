6306edfa7584a269dc562318fa414088635f3985
Glitch (hello-express) <none>
2024-12-18 09:28:02 +0000

    🚡🍕 Checkpoint ./home.html:30726734/238 ./menu.html:30726734/1523 ./main.js:30726734/368

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-fetch('menu.html')
-  .then(response => response.text())
-  .then(data => {
-    const menuContainer = document.getElementById('menu-container'); 
-    menuContainer.innerHTML = data; 
-  });
 
-$(document).ready(function() {
-  $('.menu-button').click(function() {
-  $(this).toggleClass('open');
-  $('.menu').toggleClass('open');
-    $('.page').toggleClass('gcover');
-	});
-});
